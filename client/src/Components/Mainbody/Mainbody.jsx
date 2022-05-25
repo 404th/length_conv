d8a6573d5067b5ad454edbe9fcb563f623895895
404th <umarov.doniyor.2002@gmail.com>
2022-05-25 12:18:04 +0500

    Some changes

--- client/src/Components/Mainbody/Mainbody.jsx
+++ client/src/Components/Mainbody/Mainbody.jsx
@@ -1,7 +1,41 @@
 import "./Mainbody.css"
+import { useState } from "react"
 
 function Mainbody () {
-    return (
+    
+	const { imperial, setImperial } = useState({
+		km: 0,
+		m: 0,
+		dm: 0,
+		sm: 0,
+		mm: 0,
+	})
+
+	const { imp, setImp } = useState({
+		km: 0,
+		m: 0,
+		dm: 0,
+		sm: 0,
+		mm: 0,
+	})
+
+	function insertImperial(e){
+		let value = e.target.value
+
+		if (value == Math.E ) {
+			console.log("exponential")
+			return
+		}
+
+		if (value < 0) {
+			console.log("Number cannot be less than 0")
+			return
+		}
+
+		console.log(value)
+	}
+
+	return (
         <div className={"main"}>
 				<div className={"convertor"}>
 					<div className={"convertor_header"}>
@@ -15,14 +49,16 @@ function Mainbody () {
 					<div className={"convertor_body"}>
 						<input
 							className={"l_input l_input_1"}
-							placeholder={"0.00"}
-							value={"0"}
+							placeholder={0}
+							onChange={e => insertImperial(e)}
+							type={"number"}
+							defaultValue={ imp.km }
 						/>
 						<select
 							className={"l_select l_select_1"}
 							name='imperial'
 							id='imperial'>
-							<option value='km'>km</option>
+							<option default  value='km'>km</option>
 							<option value='m'>m</option>
 							<option value='dm'>dm</option>
 							<option value='sm'>sm</option>
@@ -30,15 +66,16 @@ function Mainbody () {
 						</select>
 						<input
 							className={"l_input l_input_2"}
-							placeholder={"0.00"}
-							value={"0"}
+							placeholder={0}
+							defaultValue={imp.m}
+							type={"number"}
 						/>
 						<select
 							className={"l_select l_select_2"}
 							name='metric'
 							id='metric'>
 							<option value='km'>km</option>
-							<option value='m'>m</option>
+							<option default value='m'>m</option>
 							<option value='dm'>dm</option>
 							<option value='sm'>sm</option>
 							<option value='mm'>mm</option>
